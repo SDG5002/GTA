932583c874aa07a11bb134341953fb576cd78415
Sudesh <sudeshgosavi5002@gmail.com>
2025-08-15 17:06:32 +0530

    Added static.json to solve the issue that on render when i refresh page it is saying not found

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['gta-frontend.onrender.com']
+  server: {
+    allowedHosts: ['https://gta-exams.onrender.com/', 'http://localhost:5173/'  ]
   }
 })
